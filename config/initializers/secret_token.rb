81f2b809011cf42e4b358aef0694297dadd028c3
bomb1e <ericgithinjimaina@gmail.com>
2015-09-19 15:05:22 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Talkpreneur::Application.config.secret_key_base = 'e1c50be33ceb1737d701aecd5ae6510e7537a34c137c707860bdcc0103b7f99abc58cbbb57548c5dc21b5c761a01993e7f661c3d4fe9e7e339377d9c7857d852'
