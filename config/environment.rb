2037a954824784f72beec0c358be77f3858798a4
bomb1e <ericgithinjimaina@gmail.com>
2015-11-09 11:38:43 +0000

    Upgraded to rails 4.2.4 and configured sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Talkpreneur::Application.initialize!
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => :plain,
+	:user_name => ENV['bomb1e'],
+	:password => ENV['We5.some'],
+	:domain => 'heroku.com',
+	:enable_startstls_auto => true
+}
